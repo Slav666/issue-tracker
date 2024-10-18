5d57494d83e8abb5843f7daccacff8a7437890c9
Slawomir Dyk <slawomir.dyk@astrosat.net>
2024-10-18 22:25:43 +0100

    customizing the radix ui theme and add a mark down editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,14 @@
+"use client";
 import React from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 import { TextField, TextArea, Button } from "@radix-ui/themes";
 
 const NewIssuesPage = () => {
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root placeholder="title"></TextField.Root>
-      <TextArea placeholder="Description" />
+      <SimpleMDE placeholder="Description" />
       <Button>Submit New Issue</Button>
     </div>
   );
