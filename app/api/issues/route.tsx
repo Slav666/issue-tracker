b18a2b3ef67b377d310de2d433e6854371c2f833
Slawomir Dyk <slawomir.dyk@astrosat.net>
2024-10-21 20:16:04 +0100

    build the edit issue page and api for updating issue

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 import { schema } from "../../ValidationSchema";
 
+
+
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = schema.safeParse(body);
