d4c53095b4174677ccc75c7f863f4b1e05d25384
Slawomir Dyk <slawomir.dyk@astrosat.net>
2024-10-26 20:10:05 +0100

    add log in and log out links

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import "@radix-ui/themes/styles.css";
 import "./theme-config.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Theme, ThemePanel, Container } from "@radix-ui/themes";
+import { Theme, Container } from "@radix-ui/themes";
+import AuthProvider from "./auth/Provider";
 import NavBar from "./NavBar";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
@@ -21,12 +22,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme>
-          <NavBar />
-          <main className="p-5">
-            <Container>{children}</Container>
-          </main>
-        </Theme>
+        <AuthProvider>
+          <Theme>
+            <NavBar />
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
