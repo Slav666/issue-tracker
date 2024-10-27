7bd6e0375b7010791cc67875d729095a63dcb5c7
Slawomir Dyk <slawomir.dyk@astrosat.net>
2024-10-27 19:38:54 +0000

    replace useEffect for react query for fetching users

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -4,17 +4,22 @@ import React, { useState, useEffect } from "react";
 import { Select } from "@radix-ui/themes";
 import axios from "axios";
 import { User } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data } = await axios.get<User[]>("/api/users");
-      setUsers(data);
-    };
-    fetchUsers();
-  }, []);
+  // if (isLoading) return skeleton
+  if (error) return null;
 
   return (
     <Select.Root>
@@ -22,7 +27,7 @@ const AssigneeSelect = () => {
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {users.map((user) => (
+          {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
             </Select.Item>
