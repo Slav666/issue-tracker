0733fd86a8c352efc7a03a42f56af8f265383259
Slawomir Dyk <slawomir.dyk@astrosat.net>
2024-10-27 17:46:16 +0000

    refactor the Navbar component

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -1,8 +1,9 @@
 import GithubProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/prisma/client";
+import { NextAuthOptions } from "next-auth";
 
-const authOptions = {
+const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
