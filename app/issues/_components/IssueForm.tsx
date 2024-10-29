f8f1f425aca6915efaace7eb8aa84996757c12b9
Slawomir Dyk <slawomir.dyk@astrosat.net>
2024-10-29 18:18:27 +0000

    add router refresh to the issue form

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -48,6 +48,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
             if (issue) await axios.patch("/api/issues/" + issue.id, data);
             else await axios.post("/api/issues", data);
             router.push("/issues");
+            router.refresh();
           } catch (error) {
             setIsSubmitted(false);
             setError("Unexpected error occurred.");
