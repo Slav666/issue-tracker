c79fe9b83bcae202fe3b8450d275be903cdde148
Slawomir Dyk <slawomir.dyk@astrosat.net>
2024-11-01 09:19:57 +0000

    build the issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
 
 export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const in_progress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+
   return (
     <div>
       {/* @ts-expect-error Server Component */}
       <LatestIssues />
+      <IssueSummary open={open} inProgress={in_progress} closed={closed} />
     </div>
   );
 }
