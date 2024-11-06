3ed2ecd0e6d53fb4a438c0205602220753121ccd
Slawomir Dyk <slawomir.dyk@astrosat.net>
2024-11-06 16:14:03 +0000

    refactor latestIssue component code to fix typescript error

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,20 @@ import prisma from "@/prisma/client";
 import IssueChart from "./IssueChart";
 import { Flex, Grid } from "@radix-ui/themes";
 import { Metadata } from "next";
-
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
   const in_progress = await prisma.issue.count({
     where: { status: "IN_PROGRESS" },
   });
   const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
-
+  // Fetch issues for LatestIssues component
+  const issues = await prisma.issue.findMany({
+    orderBy: { createdAt: "desc" },
+    take: 5,
+    include: {
+      assignedToUser: true,
+    },
+  });
   return (
     <div>
       <Grid columns={{ initial: "1", md: "2" }} gap="5">
@@ -20,13 +26,12 @@ export default async function Home() {
           <IssueSummary open={open} inProgress={in_progress} closed={closed} />
           <IssueChart open={open} inProgress={in_progress} closed={closed} />
         </Flex>
-        {/* @ts-expect-error Server Component */}
-        <LatestIssues />
+        {/* Pass issues as props to LatestIssues */}
+        <LatestIssues issues={issues} />
       </Grid>
     </div>
   );
 }
-
 export const metadata: Metadata = {
   title: "Issue Tracker - Dashboard",
   description: "View a summary of project issues",
